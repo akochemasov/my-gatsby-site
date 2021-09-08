daaf15967699ec201f87fc6d8768af52a1c4bfa6
akochemasov <alexander.kochemasov@litota.ru>
2021-09-08 18:36:24 +0400

    Add CONTENTFUL_KEY

--- src/components/Test.js
+++ src/components/Test.js
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types'
 function Test({data}) {
   console.log('GATSBY_API_KEY', process.env.GATSBY_API_KEY);
   console.log('CLOUD_KEY', process.env.CLOUD_KEY);
+  console.log('CONTENTFUL_KEY', process.env.CONTENTFUL_KEY);
 
   return (
     <h1>
-      {data} - {process.env.GATSBY_API_KEY} - {process.env.CLOUD_KEY}
+      {data} - {process.env.GATSBY_API_KEY} - {process.env.CLOUD_KEY} - {process.env.CONTENTFUL_KEY}
     </h1>
   )
 }
