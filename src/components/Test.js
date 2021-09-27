50c7bdcbb1ec3e81adbe07cc8ab45a38b5b781c2
Kochemasov Alexander <alexander.kochemasov@litota.ru>
2021-09-27 16:56:07 +0400

    Envs (#2)
    Read envs

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,9 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function Test({data}) {
+  console.debug('GATSBY_API_KEY', process.env.GATSBY_API_KEY);
+  console.debug('GATSBY_CLOUD_KEY', process.env.GATSBY_CLOUD_KEY);
+
   return (
     <h1>
-      {data}
+      {data} - {process.env.GATSBY_API_KEY} - {process.env.GATSBY_CLOUD_KEY}
     </h1>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -149,7 +149,9 @@ const IndexPage = () => {
           😎
         </span>
       </p>
+
       <Test data={'Test'} />
+
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
@@ -179,19 +181,20 @@ const IndexPage = () => {
         ))}
       </ul>
 
-      {/* Использовать в крайних случаях, так как есть определенные минусы в использовании папки static */}
-      <img src={'/logo2.png'} alt="Logo" style={{width: '20px', height: '20px', border: '1px solid blue'}} />
+      <div>
+        <div>Logos:</div>
 
-      {/* Использовать в крайних случаях, так как есть определенные минусы в использовании папки static */}
-      <img src={'/logo.png'} alt="Logo" style={{width: '20px', height: '20px', border: '1px solid blue'}} />
+        {/* Использовать в крайних случаях, так как есть определенные минусы в использовании папки static */}
+        <img src={'/logo2.png'} alt="Logo" style={{width: '20px', height: '20px', border: '1px solid blue'}} />
 
-      {/* Рекомендованный подход */}
-      <img src={Icon} alt="Logo" style={{width: '20px', height: '20px', border: '1px solid red'}}/>
+        {/* Рекомендованный подход */}
+        <img src={Icon} alt="Logo" style={{width: '20px', height: '20px', border: '1px solid red'}}/>
 
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+        <img
+          alt="Gatsby G Logo"
+          src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+        />
+      </div>
     </main>
   )
 }
