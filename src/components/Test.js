3ac982366c24aa4f27db2c76b40d892ba5bbda15
akochemasov <alexander.kochemasov@litota.ru>
2021-09-29 11:28:43 +0400

    Test for lint

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Test({data}) {
+function Test({ data }) {
+  const [state, setState] = useState('');
   console.debug('GATSBY_API_KEY', process.env.GATSBY_API_KEY);
   console.debug('GATSBY_CLOUD_KEY', process.env.GATSBY_CLOUD_KEY);
 
+  useEffect(() => {
+    setState(data);
+  }, []);
+
   return (
     <h1>
-      {data} - {process.env.GATSBY_API_KEY} - {process.env.GATSBY_CLOUD_KEY}
+      {data} - {state} - {process.env.GATSBY_API_KEY} - {process.env.GATSBY_CLOUD_KEY}
     </h1>
-  )
+  );
 }
 
 Test.propTypes = {
   data: PropTypes.string
-}
+};
 
-export default Test
-
+export default Test;
